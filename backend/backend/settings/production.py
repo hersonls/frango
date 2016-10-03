91902f0291a117615e518951396158ea5cc64a83
mattjegan <matthewj.egan@hotmail.com>
2016-10-04 10:40:09 +1100

    Moved frontend dir references to dev and prod settings

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -1,3 +1,31 @@
 from backend.backend.settings.base import *
 
 DEBUG = False
+
+FRONTEND_DEV = True
+FRONTEND_ENV = ('' if FRONTEND_DEV else 'dist')
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(FRONTEND_DIR, FRONTEND_ENV, 'templates')
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+STATICFILES_DIRS = [
+    os.path.join(FRONTEND_DIR, FRONTEND_ENV, 'static'),
+]
+
+if FRONTEND_DEV:
+    STATICFILES_DIRS.insert(0, os.path.join(FRONTEND_DIR, '.tmp'))
