5f1af3e1f1365d35d1b8a751d9af6b535c3762ab
Hersonls <hersonls@gmail.com>
2017-05-28 23:28:01 -0300

    Fix env and added production static settings

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -1,5 +1,11 @@
 from .base import *
 
+import dj_database_url
+
+# Heroku Database
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -18,6 +24,10 @@ TEMPLATES = [
     },
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(FRONTEND_DIR, 'dist', 'static'),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
