b275b882fa516f958fc87a940f0167f68e210684
Herson Leite <hersonls@gmail.com>
2017-07-14 11:11:07 -0300

    Refactored server command
    Refactored server command to support different task runners

--- backend/apps/frontend/management/commands/server.py
+++ backend/apps/frontend/management/commands/server.py
@@ -3,34 +3,54 @@ import subprocess
 import atexit
 import signal
 
+from glob import glob
+
 from django.conf import settings
 from django.contrib.staticfiles.management.commands.runserver import Command\
     as StaticfilesRunserverCommand
 
 
 class Command(StaticfilesRunserverCommand):
+    frontend_dir = getattr(settings, 'FRONTEND_DIR', '')
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.taskrunner = self.get_taskrunner_command()
+
+    def get_taskrunner_command(self):
+        taskrunner = ''
+
+        if glob(os.path.join(self.frontend_dir, 'gulpfile*js')):
+            taskrunner = 'gulp'
+
+        if glob(os.path.join(self.frontend_dir, 'Gruntfile*js')):
+            taskrunner = 'grunt'
+
+        return taskrunner
 
     def inner_run(self, *args, **options):
         self.start_grunt()
         return super(Command, self).inner_run(*args, **options)
 
     def start_grunt(self):
-        self.stdout.write('>>> Starting gulp server:django task...')
+        self.stdout.write('>>> Starting {0} server:django task...'.format(
+                          self.taskrunner))
+
+        command = ['{0} serve:django --cwd {1}'.format(self.taskrunner,
+                   self.frontend_dir)]
 
         self.grunt_process = subprocess.Popen(
-            ['gulp serve:django --cwd {0}'
-             .format(settings.FRONTEND_DIR)],
+            command,
             shell=True,
             stdin=subprocess.PIPE,
             stdout=self.stdout,
             stderr=self.stderr,
         )
 
-        self.stdout.write('>>> Gulp process on pid {0}'
-                          .format(self.grunt_process.pid))
-
         def kill_grunt_process(pid):
-            self.stdout.write('>>> Closing gulp process')
+            self.stdout.write('>>> Closing {0} process'.format(
+                              self.frontend_dir))
+
             os.kill(pid, signal.SIGTERM)
 
         atexit.register(kill_grunt_process, self.grunt_process.pid)
